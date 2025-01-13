c8b47bbf7c3407b131a188e8c11dddd5a921e4cc
CRAFT1226 <yanchaun0970@gmail.com>
2025-01-13 15:06:03 +0800

    TEST

--- piano_reservation/settings.py
+++ piano_reservation/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles"  # collectstatic 命令的目標目錄
+STATIC_ROOT = BASE_DIR / "static"  # collectstatic 命令的目標目錄
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
