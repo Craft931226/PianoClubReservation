b99c4e02e9f5d08f2173aff9641f60340335f4b9
CRAFT1226 <yanchaun0970@gmail.com>
2025-01-13 15:42:56 +0800

    test7

--- reservation/views.py
+++ reservation/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'reservation/home.html')
+    return render(request, 'home.html')
 def second_page(request):
     return render(request, 'second.html')
