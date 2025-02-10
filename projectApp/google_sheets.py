18a3d9009395befad9966391d186ea4a345416ab
CRAFT1226 <yanchaun0970@gmail.com>
2025-02-10 19:33:40 +0800

    update Profile Page, gmail no use

--- projectApp/google_sheets.py
+++ projectApp/google_sheets.py
@@ -5,21 +5,21 @@ import os
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
-# # 配置 Google Sheets API 憑證
-# SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-# # 從環境變數中加載憑證
-# credentials_info = json.loads(os.getenv('GOOGLE_CREDENTIALS_JSON'))
-# credentials = Credentials.from_service_account_info(credentials_info, scopes=SCOPES)
-# SPREADSHEET_ID = os.getenv('GOOGLE_SHEET_ID')  # 修改為實際試算表的 ID
-# service = build('sheets', 'v4', credentials=credentials)
-
-
-# 配置 Google Sheets API 憑證(本地端)
+# 配置 Google Sheets API 憑證
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SERVICE_ACCOUNT_FILE = r"C:\Users\User\Downloads\skilled-script-448314-j0-0c05f20ca146.json"
-credentials = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+# 從環境變數中加載憑證
+credentials_info = json.loads(os.getenv('GOOGLE_CREDENTIALS_JSON'))
+credentials = Credentials.from_service_account_info(credentials_info, scopes=SCOPES)
+SPREADSHEET_ID = os.getenv('GOOGLE_SHEET_ID')  # 修改為實際試算表的 ID
 service = build('sheets', 'v4', credentials=credentials)
-SPREADSHEET_ID = '1TWp1XNDx46NPLRZQA9AVA1yghVxhA_2TZwwb9c0fIMA'
+
+
+# # 配置 Google Sheets API 憑證(本地端)
+# SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+# SERVICE_ACCOUNT_FILE = r"C:\Users\User\Downloads\skilled-script-448314-j0-0c05f20ca146.json"
+# credentials = Credentials.from_service_account_file(SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+# service = build('sheets', 'v4', credentials=credentials)
+# SPREADSHEET_ID = '1TWp1XNDx46NPLRZQA9AVA1yghVxhA_2TZwwb9c0fIMA'
 
 
 # update_user_name 函數將用戶名更新到試算表中
