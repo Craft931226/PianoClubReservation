dd1921a6e1c5fef6e24b09bd3060e5147b6d6cd8
CRAFT1226 <yanchaun0970@gmail.com>
2025-04-02 11:42:52 +0800

    modify path

--- test_project/settings.py
+++ test_project/settings.py
@@ -129,10 +129,8 @@ USE_TZ = True
 
 # This setting informs Django of the URI path from which your static files will be served to users
 # Here, they well be accessible at your-domain.onrender.com/static/... or yourcustomdomain.com/static/...
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),  # 假如你有 global static/
-]
+STATIC_URL = 'projectApp/static/'
+
 
 # This production code might break development mode, so we check whether we're in DEBUG mode
 if not DEBUG:
